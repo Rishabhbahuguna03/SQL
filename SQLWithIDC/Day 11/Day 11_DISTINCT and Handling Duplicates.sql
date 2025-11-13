0f26e019b2801bb3ff3014f7f2f3e00e3f8f3fbc
Rishabh Bahuguna <87006865+Rishabhbahuguna03@users.noreply.github.com>
2025-11-14 00:20:56 +0530

    Add files via upload

---
+++ SQLWithIDC/Day 11/Day 11_DISTINCT and Handling Duplicates.sql
@@ -0,0 +1,29 @@
+-- Practice questions:
+-- Q.1. List all unique services in the patients table.
+
+SELECT DISTINCT(service)
+FROM patients;
+
+-- Q.2. Find all unique staff roles in the hospital.
+
+SELECT DISTINCT(role)
+FROM staff;
+
+-- Q.3. Get distinct months from the services_weekly table.
+
+SELECT DISTINCT month
+FROM services_weekly;
+
+-- Day 11 Challenge Question:
+-- Find all unique combinations of service and event type from the services_weekly table where events are not null or none, along with the count of occurrences for each combination. Order by count descending.
+
+SELECT 
+    service,
+    event,
+    COUNT(*) AS event_count
+FROM services_weekly
+WHERE event IS NOT NULL
+  AND event != 'None'
+GROUP BY service, event
+ORDER BY event_count DESC;
+
