b27f41f355b981f3f2be5d0be00e4960064f8193
Rishabh Bahuguna <87006865+Rishabhbahuguna03@users.noreply.github.com>
2025-11-21 09:51:49 +0530

    Add files via upload

---
+++ SQLWithIDC/Day 16/Day 16_Subqueries.sql
@@ -0,0 +1,52 @@
+-- Practice questions:
+-- Q.1. Find patients who are in services with above-average staff count.
+
+SELECT *
+FROM patients
+WHERE service IN
+(SELECT service
+FROM staff
+GROUP BY service
+HAVING COUNT(staff_id) >
+(SELECT AVG(staff_count)
+FROM (SELECT COUNT(staff_id) AS staff_count 
+FROM staff
+GROUP BY service) AS service_counts));
+
+-- Q.2. List staff who work in services that had any week with patient satisfaction below 70.
+
+SELECT DISTINCT *
+FROM staff
+WHERE service IN
+(SELECT service
+FROM services_weekly
+WHERE patient_satisfaction < 70);
+
+-- Q.3. Show patients from services where total admitted patients exceed 1000.
+
+SELECT *
+FROM patients
+WHERE service IN
+(SELECT service
+FROM services_weekly
+GROUP BY service
+HAVING SUM(patients_admitted) > 1000);
+
+
+-- Day 16 Challenge Question:
+--  Find all patients who were admitted to services that had at least one week where patients were refused AND the average patient satisfaction for that service was below the overall hospital average satisfaction. Show patient_id, name, service, and their personal satisfaction score.
+
+SELECT patient_id, name, service, satisfaction
+FROM patients
+WHERE service IN
+(SELECT service
+FROM services_weekly
+GROUP BY service
+HAVING 
+SUM(CASE
+		WHEN patients_refused > 0 THEN 1
+		ELSE 0
+		END) >= 1
+AND AVG(patient_satisfaction) <
+(SELECT AVG(patient_satisfaction) 
+FROM services_weekly));
